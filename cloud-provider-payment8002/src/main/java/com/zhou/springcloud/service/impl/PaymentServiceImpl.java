29ee833e58c6e40f965d429e3a6fb638e9070eb2
YWAnonymous <1274315645@qq.com>
2020-11-09 13:55:57 +0800

    moddif

---
+++ cloud-provider-payment8002/src/main/java/com/zhou/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.zhou.springcloud.service.impl;
+
+import com.zhou.springcloud.dao.PaymentDao;
+import com.zhou.springcloud.entities.Payment;
+import com.zhou.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
