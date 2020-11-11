0f7717e4b00a093cdecc6faee22f4daf741c451f
YWAnonymous <1274315645@qq.com>
2020-11-11 13:26:59 +0800

    降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/zhou/springcloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/zhou/springcloud/OrderHystrixMain80.java
@@ -2,12 +2,10 @@ package com.zhou.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
 public class OrderHystrixMain80 {
--- cloud-provider-hystrix-payment8001/src/main/java/com/zhou/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/zhou/springcloud/controller/PaymentController.java
@@ -1,5 +1,7 @@
 package com.zhou.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.zhou.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,9 +24,26 @@ public class PaymentController {
     }
 
     @GetMapping("/payment/hystrix/timeout/{id}")
+//    @HystrixCommand(fallbackMethod = "payment_TimeOutHandler", commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")}
+//    )
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentService.paymentInfo_TimeOut(id);
-        log.info("*****result: " + result);
+        log.info("*****timeout: " + result);
+        return result;
+    }
+
+    public String payment_TimeOutHandler(Integer id) {
+        return "调用接口报错 \t" + "\t 当前线程" + Thread.currentThread().getName();
+    }
+
+
+    //====服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id)
+    {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("****result: "+result);
         return result;
     }
 }
