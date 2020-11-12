bbb703c2691ec22286afc93b716f0c15dd38e36f
YWAnonymous <1274315645@qq.com>
2020-11-12 17:32:19 +0800

    springcloud配置

--- cloud-provider-payment8001/src/main/java/com/zhou/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/zhou/springcloud/controller/PaymentController.java
@@ -52,4 +52,9 @@ public class PaymentController {
         }
         return serverPort;
     }
+
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB() {
+        return serverPort;
+    }
 }
